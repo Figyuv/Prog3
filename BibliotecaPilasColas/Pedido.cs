30be8dfd0e934aff3b84b8d2ce1a7ebb1dcf66b7
David Calvi <david.calvi.18@gmail.com>
2022-02-25 11:30:50 -0400

    25022022 practica pilas y colas

---
+++ BibliotecaPilasColas/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaPilasColas
+{
+    internal class Pedido
+    {
+        public int Telefono { get; set; }
+        public List<string> Producto { get; set; }
+
+        public Pedido(int telefono, List<string> products)
+        {
+            Telefono = telefono;
+            Producto = products;
+            
+        }
+
+
+
+    }
+}
