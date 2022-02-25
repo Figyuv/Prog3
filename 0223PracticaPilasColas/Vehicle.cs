e8b3169f10a5d4a189e9f9083a5cb01460fb8bc4
David Calvi <david.calvi.18@gmail.com>
2022-02-24 23:46:27 -0400

    practica pilas colas

---
+++ 0223PracticaPilasColas/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0223PracticaPilasColas
+{
+    internal class Vehicle
+    {
+        public string Id { get; set; }
+        public string Color { get; set; }
+
+        public Vehicle(string id, string color)
+        {
+            Id = id;
+            Color = color;
+        }
+    }
+}
