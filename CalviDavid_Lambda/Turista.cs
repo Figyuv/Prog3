e829848b01990b13942e627cca34a99683862115
David Calvi <david.calvi.18@gmail.com>
2022-04-22 00:18:42 -0400

    LinqLambda

---
+++ CalviDavid_Lambda/Turista.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalviDavid_Lambda
+{
+    internal class Turista
+    {
+        public int CIT;
+        public string NombreT;
+        public string CodTo;
+        public int CodP;
+
+        public Turista(int cIT, string nombreT, string codTo, int codP)
+        {
+            CIT = cIT;
+            NombreT = nombreT;
+            CodTo = codTo;
+            CodP = codP;
+        }
+    }
+}
